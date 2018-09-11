962d84f66bce80368f246b475bbabf72939f98f0
sechaba.mothobi <sechaba.mothobi@investec.co.za>
2018-09-11 11:25:53 +0200

    v6_variables

--- C#Basics/Assets/Scripts/FirstScript.cs
+++ C#Basics/Assets/Scripts/FirstScript.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour {
 
+	int score = 10;
+	float speed = 2.5f;
+	char character = 'A';
+	string name = "Azania";
+
 	// Use this for initialization
 	void Start () {
-		print("First Name");
+		print(score);
+		print(speed);
+		print(character);
+		print(name);
 	}
 	
 	// Update is called once per frame
